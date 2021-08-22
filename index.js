3fcfca75e3eddf144d1259c599a6bae7d17f6aad
CalebT <k.cterry30@gmail.com>
2021-08-22 00:42:49 -0500

    added index.js, added sticky header, added hackathon picture, fixed pop up text h3 for layer

---
+++ index.js
@@ -0,0 +1,38 @@
+///////////////////
+// small nav menu
+///////////////////
+
+// getting id of nav links menu
+var navLinks = document.getElementById("navLinks");
+
+// function to hide menu
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
+
+// function to show menu
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
+///////////////////
+// scrolling
+///////////////////
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("navMenu");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
